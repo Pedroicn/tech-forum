609e601df75bd55c4a6b541e3b608c15ff876aa7
pedro_igor <pedro.nunes@techdog.com.br>
2023-11-30 19:30:54 -0300

    feat: create Topic constructor

--- src/TechForum.Business/Models/Topic.cs
+++ src/TechForum.Business/Models/Topic.cs
@@ -3,17 +3,27 @@ namespace TechForum.Business.Models;
 public class Topic
 {
   public Guid UserId { get; private set; }
-
   public Guid TopicId { get; private set; }
   public string Description { get; private set; }
   public string Title { get; private set; }
 
   public int CommentAmount { get; private set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
 
   private List<Comment> _comments;
 
-  public IReadOnlyCollection<Comment> comments => _comments;
+  public IReadOnlyCollection<Comment> Comments => _comments;
 
+  public Topic(User user, string title, string description)
+  {
+    UserId = user.Id;
+    TopicId = Guid.NewGuid();
+    Title = title;
+    Description = description;
+    CreatedAt = DateTime.Now;
+    UpdatedAt = DateTime.Now;
+  }
   public void AddComments(Comment comment)
   {
     CommentAmount++;
